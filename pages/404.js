61aca6b831c3a59620dec88bec971df1a0171f1c
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2022-05-08 02:14:39 +0530

    Add sentry

---
+++ pages/404.js
@@ -0,0 +1,6 @@
+import styles from '@styles/error.module.scss';
+
+
+export default function Custom404() {
+  return <div className={styles.container}><h1>404 - Page Not Found</h1></div>
+}
---
+++ pages/500.js
@@ -0,0 +1,6 @@
+import styles from '@styles/error.module.scss';
+
+
+export default function Custom404() {
+  return <div className={styles.container}><h1>500 - Server Error</h1></div>
+}
