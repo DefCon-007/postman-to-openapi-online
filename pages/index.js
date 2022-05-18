519277245ddcd03050f6ab44353b1138edf60e78
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2022-05-19 01:43:57 +0530

    Add SEO meta tags

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,11 @@ import Header from "../components/Header"
 import Main from "../components/Main"
 import Footer from "../components/Footer"
 
+const baseUrl = 'https://p2o.defcon007.com',
+    title = 'Postman to OpenAPI',
+    description = 'Convert Postman collections to Open API schema in one click.  Works locally on the browser without sending any data to any server.',
+    coverImageUrl = baseUrl + '/images/cover-image.jpg';
+
 class IndexPage extends React.Component {
     constructor(props) {
         super(props)
@@ -73,7 +78,24 @@ class IndexPage extends React.Component {
             <div className={`body ${this.state.loading} ${this.state.isArticleVisible ? "is-article-visible" : ""}`}>
                 <div>
                     <Head>
-                        <title>Postman to OpenAPI</title>
+                        {/* Primary Meta Tags */}
+                        <title>{title}</title>
+                        <meta name="title" content={title} />
+                        <meta name="description" content="" />
+
+                        {/* Open Graph / Facebook */}
+                        <meta property="og:type" content="website" />
+                        <meta property="og:url" content={baseUrl} />
+                        <meta property="og:title" content={title} />
+                        <meta property="og:description" content={description} />
+                        <meta property="og:image" content={coverImageUrl} />
+
+                        {/* Twitter */}
+                        <meta property="twitter:card" content="summary_large_image" />
+                        <meta property="twitter:url" content={baseUrl} />
+                        <meta property="twitter:title" content={title} />
+                        <meta property="twitter:description" content={description} />
+                        <meta property="twitter:image" content={coverImageUrl} />
                     </Head>
 
                     <div id="wrapper">
