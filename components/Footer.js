3677894f38b2a6fbc4d6c9ea9595b99233f0e613
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2022-06-24 23:10:50 +0530

    Add link to GitHub repo

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">Made with &hearts; by <a href="https://defcon007.com" target="_blank">Defcon</a>. Built with: <a href="https://github.com/zeit/next.js" target="_blank">Next.js</a></p>
+        <p className="copyright">Made with &hearts; by <a href="https://defcon007.com" target="_blank">Defcon</a> |  <a href="https://github.com/DefCon-007/postman-to-openapi-online" target="_blank">GitHub</a> | Built with: <a href="https://github.com/zeit/next.js" target="_blank">Next.js</a></p>
     </footer>
 )
 
